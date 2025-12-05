8bdd67917b9eed44d2863ea0e4fa8830d29ee6f4
sivabasani95 <sivabasani95@gmail.com>
2025-12-05 13:58:25 -0600

    Update stringUtils.js

---
+++ string-utilities/stringUtils.js
@@ -0,0 +1,17 @@
+// to capitalize  the  first letter of string.
+
+export function capitalize(string) {
+if (!string) return " ";
+return string.charAt(0).toUpperCase()+string(1);
+}
+
+// to reverse sting 
+
+export function reverse(sting) {
+return sting.split("").reverse().join("") ;
+}
+// check substing is existed in string?
+
+export function contain(substing, str) {
+    return include (substing);
+}
