dece058873016c692665fea0ca57b84680b60187
Abhir <sivabasani95@gmail.com>
2025-12-05 15:34:39 -0600

    Resolved merge conflicts

--- string-utilities/app.js
+++ string-utilities/app.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
-import { capitalize, reverse, contain } from './string-utilities.js';
+
+import { capitalize, reverse, contain } from './stringUtils.js';
 
 console.log(capitalize("hello-world"));   // "Hello-world"
 console.log(reverse("whitecat"));         // "tacehtiw"
 console.log(contain("cat", "whitecat"));  // true
 console.log(contain("dog", "whitecat"));  // false
-=======
 
->>>>>>> 7f401a1479d3ec2374a20a0fc7bbae5e03849ef5
+
+
